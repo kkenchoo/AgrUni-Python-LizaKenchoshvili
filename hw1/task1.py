47a950be67383d68ac16a9d43e3a9b5d172edbad
lizza kenchoshvili <lizikokenchoshvili@live.com>
2025-10-22 21:24:13 +0400

    first hw

---
+++ hw1/task1.py
@@ -0,0 +1,13 @@
+a = float(input("სამკუთხედის პირველი გვერდი: "))
+b = float(input("სამკუთხედის მეორე გვერდი: "))
+c = float(input("სამკუთხედის მესამე გვერდი: "))
+
+perimeter = a + b + c
+semiperimeter = perimeter/2
+
+if a + b > c and a + c > b and b + c > a:
+    area = (semiperimeter * (semiperimeter - a) * (semiperimeter - b) * (semiperimeter - c)) ** 0.5
+    print(f"\nსამკუთხედის ფართობი არის: {area:.2f}")
+    print(f"სამკუთხედის პერიმეტრი არის: {perimeter}")
+else:
+    print("სამკუთხედი არ იკვრება")
