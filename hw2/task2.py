545cea77a4bb3ab6a2ba7352a3bb881950c9b4df
lizza kenchoshvili <lizikokenchoshvili@live.com>
2025-11-09 16:09:01 +0400

    second hw

---
+++ hw2/task2.py
@@ -0,0 +1,11 @@
+subjects = {
+    'math': {'George': 85, 'Salome': 78, 'David': 92},
+    'physics': {'George': 90, 'David': 75, 'Salome': 88},
+    'chemistry': {'David': 82, 'George': 80, 'Salome': 91}
+}
+
+#Output: {'George': {'math': 85, 'physics': 90, 'chemistry': 80}, ...}
+
+students = {}
+subjects_grades = {}
+
